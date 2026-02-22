480aa1c9ed8b89158eabd3b8d133e7ff506895c4
RoihanTauhid <roihanntauhidd@gmail.com>
2026-02-23 00:15:10 +0700

    login-register-Done

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import "../styling/login.css"
+import logo from "/public/movie-open.png"
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div><h1>Halaman Login</h1></div>
+  <section>
+    <div className="login-box">
+        <div className="login-box-desc">
+          <p className="judul"> <img  className="logo-login" src={logo} alt="" /> CHILL</p>
+          <h3>Masuk</h3>
+          <h6>Selamat Datang Kembali</h6>
+          <form action="">
+              <label className="username" htmlFor="username">Username</label>
+              <input type="text" placeholder="Masukkan Username"/>
+              <br/>
+              <label className="password" htmlFor="password">Kata Sandi</label>
+              <div className="password-wrapper">
+                  <input type="password" id="password" placeholder="Masukkan Kata Sandi" />
+                  <i className="fa-solid fa-eye"></i>
+              </div>   
+          </form>
+          <div className="daftar">
+              <div className="daftar1"><p>Belum Punya Akun? <Link to="/register" className="daftar-link" >   Daftar </Link> </p></div>
+              <div className="daftar2"><a href="#" className="lupa-sandi">Lupa Kata Sandi?</a></div>
+          </div>
+          <div className="masuk-btn">
+              <button className="btn-masuk-1"><a href="./beranda.html">Masuk</a> </button>
+              <p>Atau</p>
+              <button className="btn-masuk-google">
+                  <div><img className="img-btn-google" src="../asset/google-img.png" alt="" /></div> 
+                  <p> Masuk Dengan Google</p> </button>
+          </div>
+        </div>
+    </div>
+  </section>
   )
 }
 
--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import "../styling/register.css"
+import { Link } from 'react-router-dom'
 
 function Register() {
   return (
-    <div><h1>Halaman Register</h1></div>
+      <section className="register-section">
+        <div className="register-box">
+            <div className="register-box-desc">
+            <p className="judul"> <img  className="logo-register" src="/public/movie-open.png" alt=""/> CHILL</p>
+            <h3>Daftar</h3>
+            <p>Selamat Datang!</p>
+            <form action="">
+                <label className="username" htmlFor="username">Username</label>
+                <input type="text" placeholder="Masukkan Username"/>
+                <br/>
+                <label className="password" htmlFor="password">Kata Sandi</label>
+                <div className="password-wrapper">
+                    <input type="password" id="password" placeholder="Masukkan Kata Sandi"/>
+                    <i className="fa-solid fa-eye"></i>
+                </div>
+                <br/>
+                <label className="password" htmlFor="password">Konfirmasi Kata Sandi</label>
+                <div className="password-wrapper">
+                    <input type="password" id="password" placeholder="Masukkan Kata Sandi"/>
+                    <i className="fa-solid fa-eye"></i>
+                </div>
+            </form>
+            <div className="punya">
+                <div className="punya1"><p>Sudah Punya Akun? <Link className="punya-link" to="/" > Masuk</Link>
+                </p></div>
+            </div>
+            <div className="daftar-btn">
+                <button className="btn-daftar-1">Daftar</button>
+                <p>Atau</p>
+                <button className="btn-daftar-google">
+                    <div><img className="img-btn-google" src="../asset/google-img.png" alt=""/></div> 
+                    <p> Daftar Dengan Google</p> </button>
+            </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
